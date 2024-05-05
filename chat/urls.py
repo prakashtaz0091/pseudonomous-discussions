0863d753f48431888d233e0ea26ff3723618fc95
prakashtaz0091 <code.ing2468@gmail.com>
2024-05-05 11:58:52 +0545

    room message done

--- chat/urls.py
+++ chat/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 
 from . import views
 
-
+app_name = "chat"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:room_name>/", views.room, name="room"),
+    # path("<str:room_name>/", views.room, name="room"),
+    path("room/", views.room, name="room"),
 
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,14 @@
 # chat/views.py
 from django.shortcuts import render
-
+from . import models
 
 def index(request):
     return render(request, "chat/index.html")
 
 
 
-def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name": room_name})
+# def room(request, room_name):
+def room(request):
+    room_name = request.GET.get("room_name", None)
+    room = models.DiscussionRoom.objects.get(name=room_name)
+    return render(request, "chat/room.html",{"room_id": room.id})
